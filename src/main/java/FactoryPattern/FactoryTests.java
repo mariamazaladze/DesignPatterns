12bcf76835339fc96c0bfee4aa628dfdaf1b28bc
MAzaladze <mariamazaladze@yahoo.com>
2021-08-18 13:12:57 +0400

    last

--- src/main/java/FactoryPattern/FactoryTests.java
+++ src/main/java/FactoryPattern/FactoryTests.java
@@ -1,33 +1,27 @@
-package FactoryPattern;
-
-import Utils.ChromeAndWeb;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
-
-public class FactoryTests {
-    private WebDriver driver;
-    ChromeAndWeb openn=new ChromeAndWeb();
-    FactoryChrome factorOpen=new FactoryChrome( driver);
-    FactoryMainPage f =new FactoryMainPage( driver );
-
-
-    public FactoryTests (WebDriver driver){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
-    }
-
-
-
-
-@Test
-    public void setMainPage(){
-
-    openn.openchrome();
-    f.formChlick();
-
-
-}
+//package FactoryPattern;
+//
+//import Utils.ChromeAndWebb;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.support.PageFactory;
+//import org.testng.annotations.Test;
+//
+//public class FactoryTestss {
+//    private WebDriver driver;
+//    ChromeAndWebb openn=new ChromeAndWebb();
+//    FactoryChrome factorOpen=new FactoryChrome( driver);
+//    FactoryMainPage f =new FactoryMainPage( driver );
+//
+//
+//    public FactoryTests (WebDriver driver){
+//        this.driver=driver;
+//        PageFactory.initElements(driver,this);
+//    }
+//
+//
+//
+//
+//
+//}
 
 
 
@@ -36,6 +30,6 @@ public class FactoryTests {
 //        formsElement.click();
 //        LeftPannelMethods left;
 //        left.LeftPannelClick();
-    }
+//    }
 
 
